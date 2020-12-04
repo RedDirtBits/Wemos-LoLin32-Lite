189141bf5bc9d154ce887ca3cc236c9efc52c570
Scotty <reddirtplace@protonmail.com>
2020-12-04 00:33:17 -0600

    Added basic code to test with

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import utime
 import wifi
+import sensors
 
 wifi.wifi_connect()
 
 print("Hello, world!")
+print('The current temperature is:', sensors.read_ds18b20()[0])
 
-utime.sleep(10)
+utime.sleep(5)
 
 wifi.wifi_disconnect()
