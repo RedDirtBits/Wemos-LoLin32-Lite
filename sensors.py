6150580ffb863f56881aae0957256be5e123c5c6
Scotty <reddirtplace@protonmail.com>
2020-12-06 04:58:05 -0600

    Sensor read functions

--- sensors.py
+++ sensors.py
@@ -4,7 +4,15 @@ import utime
 import ds18x20
 
 
-def read_ds18b20():
+from machine import I2C, Pin
+from bh1750 import BH1750  # Light Sensor
+from bmp180 import BMP180  # Pressure, Humidity, etc. Sensor
+from ccs811 import CCS811  # Environmetal sensor
+
+bus = I2C(scl=Pin(23), sda=Pin(19), freq=100000)
+
+
+def read_ds18b20(self):
 
     sensor_wire = onewire.OneWire(machine.Pin(5))
     temperature_sensor = ds18x20.DS18X20(sensor_wire)
@@ -18,3 +26,10 @@ def read_ds18b20():
     fahrenheit = (celcius * 1.8) + 32
 
     return fahrenheit, celcius
+
+
+def read_bh1750():
+
+    sensor = BH1750(bus)
+
+    return sensor.luminance(BH1750.ONCE_HIRES_1)
