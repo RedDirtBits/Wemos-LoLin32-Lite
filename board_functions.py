80138d4f6fe972395c80ac6cd52669c2fd8b5171
Scotty <reddirtplace@protonmail.com>
2020-12-09 00:53:23 -0600

    Added function to turn off vendor debugging

--- board_functions.py
+++ board_functions.py
@@ -7,14 +7,32 @@ ESP32 board related functions.
 
 
 def get_flash():
+    """
+    get_flash: Reads the on-board Flash memory
 
-    # Returns the on-board flash size in KB
+    Returns:
+        float: On-Board Flash memory in kilobytes
+    """
 
     return esp.flash_size() / 1000
 
 
 def get_cpu_temperature():
+    """
+    get_cpu_temperature: Reads the CPU raw temperature
+
+    Returns:
+        int: CPU temperature in Fahrenheit
+    """
 
     # Returns internal MCU temperature in Fahrenheit
 
     return esp32.raw_temperature()
+
+
+def vendor_debug_off():
+    """
+    vendor_debug_off: Turns off vendor debugging messages
+    """
+
+    esp.osdebug(None)
