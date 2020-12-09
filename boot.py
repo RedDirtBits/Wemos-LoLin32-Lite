49b62924e0224a5dd42c885b57286cbffe72f334
Scotty <reddirtplace@protonmail.com>
2020-12-09 00:53:39 -0600

    Removed function from boot file

--- boot.py
+++ boot.py
@@ -1,9 +1,1 @@
 # This is script that run when device boot up or wake from sleep.
-
-import esp
-
-# Redirect debugging messages
-# esp.osdebug(0)
-
-# Turn off vendor debugging messages
-esp.osdebug(None)
